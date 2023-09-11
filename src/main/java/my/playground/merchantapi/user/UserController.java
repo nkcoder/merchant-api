0f3407b37a8068be65cd2faea106738fd8a9961e
Daniel <daniel5hbs@gmail.com>
2023-09-11 15:34:10 +1000

    fix the integration tests in controller which is not using H2

--- src/main/java/my/playground/merchantapi/user/UserController.java
+++ src/main/java/my/playground/merchantapi/user/UserController.java
@@ -2,6 +2,8 @@ package my.playground.merchantapi.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
   private final UserService userService;
 
   @PostMapping("/register")
   public ResponseEntity<User> registerUser(
       @RequestBody @Valid UserRegistrationReq registrationReq) {
+    logger.info("Received register request.");
     User registeredUser = userService.register(registrationReq);
     return new ResponseEntity<>(registeredUser.shadowPassword(), HttpStatus.CREATED);
   }
--- src/test/java/my/playground/merchantapi/user/UserControllerTest.java
+++ src/test/java/my/playground/merchantapi/user/UserControllerTest.java
@@ -15,9 +15,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 @SpringBootTest
 public class UserControllerTest {
 
