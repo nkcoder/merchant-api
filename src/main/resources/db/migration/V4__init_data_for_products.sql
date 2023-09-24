2c2aecf812510318bd5c50e90c631811a8636608
Daniel <daniel5hbs@gmail.com>
2023-09-24 12:54:42 +1000

    init data for products

---
+++ src/main/resources/db/migration/V4__init_data_for_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO products(seller_id, product_name, description, price, quantity)
+VALUES (1, 'iPhone', 'A great phone', 2000, 20),
+       (2, 'Tesla', 'The smartest car', 65000, 30),
+       (3, 'GPT-4', 'The best model from OpenAI', 1500, 100),
+       (4, 'Effective Java (3rd)', 'Awesome book about Java', 40, 5000),
+       (5, 'Functional Programming in Scala (2nd)', 'Great book about FP', 50, 30000);
